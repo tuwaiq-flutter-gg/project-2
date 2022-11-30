2fa1a0a9d6d48e9ae0f348e841878e440c9180d9
FasialG <117646880+FasialG@users.noreply.github.com>
2022-11-30 17:58:39 +0300

    Project Creation

---
+++ faisal_project2/lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Faisal_Project2());
+}
+
+class Faisal_Project2 extends StatelessWidget {
+  const Faisal_Project2({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home:(home()),
+    );
+  }
+}
+
+class home extends StatelessWidget {
+  const home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
